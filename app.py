df1b681a2a0e9f2757f83362eea9ebe4f7ee26da
Nestor2014 <nebeferreyra@gmail.com>
2024-03-20 19:57:18 -0300

    agregue parametros

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def raiz():
     titulo = "pagina inicio"
-    return render_template('inicio.html')
+    return render_template('inicio.html',titulo=titulo)
 
 # ruta para nosotros
 @app.route('/nosotros')
 def nosotros():
     titulo = "nosotros"
-    return render_template('nosotros.html')
+    return render_template('nosotros.html',titulo=titulo)
 
 # bloque de prueba
 if __name__ == "__main__":
