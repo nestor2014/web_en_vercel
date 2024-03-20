b70588184cb03564b5d6758a54f10120ee481347
Nestor2014 <nebeferreyra@gmail.com>
2024-03-19 22:33:20 -0300

    iniciando con flask y vercel

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template 
+app = Flask(__name__)
+# rutas
+@app.route('/')
+def raiz():
+    titulo = "pagina inicio"
+    return render_template('inicio.html')
+
+# ruta para nosotros
+@app.route('/nosotros')
+def nosotros():
+    titulo = "nosotros"
+    return render_template('nosotros.html')
+
+# bloque de prueba
+if __name__ == "__main__":
+    app.run(debug=True)
